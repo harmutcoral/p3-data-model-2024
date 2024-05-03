8cef83663634e59761dbed4095f1a42b63eceab0
harmutcoral <coral.harmut98@gmail.com>
2024-05-03 18:30:59 +0200

    update review script

---
+++ scripts/updatereview.ts
@@ -0,0 +1,42 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const updateReview = async () => {
+
+    const user = await prisma.user.findFirst({
+        where: { name: "Carla Fernández" }
+    });
+
+    if (user) {
+
+        const series = await prisma.series.findFirst({
+            where: { title: "Breaking Bad" }
+        });
+
+        if (series) {
+
+            const review = await prisma.review.findFirst({
+                where: {
+                    userId: user.id,
+                    seriesId: series.id
+                }
+            });
+
+            if (review) {
+
+                const updatedReview = await prisma.review.update({
+                    where: { id: review.id },
+                    data: {
+                        content: "I changed my mind, it's not that good.",
+                        score: 3 
+                    }
+                });
+
+                console.log("Review updated successfully! :)");
+            }
+        }
+    }
+};
+
+await updateReview();
