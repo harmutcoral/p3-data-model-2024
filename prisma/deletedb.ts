f05ae2ca83c536d5ec244bdf293e2dcc0e4732b6
harmutcoral <coral.harmut98@gmail.com>
2024-05-03 17:10:57 +0200

    create script to delete prisma db

---
+++ prisma/deletedb.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const deleteData = async () => {
+    await prisma.review.deleteMany();
+    await prisma.user.deleteMany();
+    await prisma.movie.deleteMany();
+};
+console.log("Data deleted successfully!")
+
+await deleteData();
