ffd511a2c4f33c52c99d152c8b349328c335d9cd
harmutcoral <coral.harmut98@gmail.com>
2024-05-03 17:47:36 +0200

    create .env file

--- prisma/deletedb.ts
+++ prisma/deletedb.ts
@@ -6,6 +6,8 @@ const deleteData = async () => {
     await prisma.review.deleteMany();
     await prisma.user.deleteMany();
     await prisma.movie.deleteMany();
+    await prisma.series.deleteMany();
+    await prisma.game.deleteMany();
 };
 console.log("Data deleted successfully! :)")
 
